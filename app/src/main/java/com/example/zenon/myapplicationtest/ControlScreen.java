e740d5caa12041e0e8ecbb0026542aacfd22f0d0
Z3non <z3non@legtux.org>
2014-11-22 11:27:40 +0100

    add list view

--- app/src/main/java/com/example/zenon/myapplicationtest/ControlScreen.java
+++ app/src/main/java/com/example/zenon/myapplicationtest/ControlScreen.java
@@ -145,6 +145,7 @@ public class ControlScreen extends Fragment {
         String files="";
         File f = new File(path);
         File file[] = f.listFiles();
+
         Log.d("Files", "Size: "+ file.length);
         for (int i=0; i < file.length; i++)
         {
